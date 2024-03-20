2c752d4d8c253b6db394708ffa811a4a844d170e
Niels Fallenbeck <niels@fallenbeck.com>
2024-03-20 20:15:05 +0100

    Add mastodon link in footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,9 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      
-
       '$ who': "https://fallenbeck.com/",
+      Mastodon: "https://social.fallenbeck.com"
       GitHub: "https://github.com/fallenbeck",
     },
   }),
